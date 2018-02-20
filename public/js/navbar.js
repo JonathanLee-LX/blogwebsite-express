45e42a750540414d908d2427876b3d1d7177cb1e
JonathanLee-LX <1150974628@qq.com>
2018-02-20 18:12:14 +0800

    disable mixed content security warning

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,9 +1,9 @@
 $(function (){
-    $(document).on("scroll", function(e){
+    $(document).on("scroll", _.debounce(function(e){
         if($(document).scrollTop() > 2*$("#my-nav").height()){
-            $("#my-nav").addClass("navbar-fixed-top")
+            $("#my-nav").addClass("navbar-fixed-top").fadeIn(300);
         }else{
             $("#my-nav").removeClass("navbar-fixed-top")
         }
-    })
+    }, 300, {'maxWait': 1200}))
 })
